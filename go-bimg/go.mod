dd584dfc700db15de9f6ef27e26581eea563a147
dltmdrbtjd <dltmdrbtjd@gmail.com>
2025-01-04 17:55:41 +0900

    feat: bimg image optimized

--- go-bimg/go.mod
+++ go-bimg/go.mod
@@ -1,3 +1,5 @@
 module github.com/dltmdrbtjd/image-bench
 
 go 1.23.4
+
+require github.com/h2non/bimg v1.1.9
--- go-bimg/main.go
+++ go-bimg/main.go
@@ -1,14 +1,99 @@
 package main
 
-func main() {
-	// imagePath := []string{
-	// 	"./assets/1.jpg",
-	// 	"./assets/2.jpg",
-	// 	"./assets/3.jpg",
-	// 	"./assets/4.jpg",
-	// 	"./assets/5.jpg",
-	// 	"./assets/6.jpg",
-	// }
+import (
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 
-	// outputPath := "./output/bimg"
+	"github.com/h2non/bimg"
+)
+
+func processImage(imagePath string, w, h int) error {
+	fileName := strings.Replace(filepath.Base(imagePath), ".jpg", ".webp", 1)
+	outputPath := filepath.Join("./output/bimg", fileName)
+
+	imageData, err := os.ReadFile(imagePath)
+	if err != nil {
+		return err
+	}
+
+	start := time.Now()
+
+	originalImage := bimg.NewImage(imageData)
+	size, err := originalImage.Size()
+	if err != nil {
+		return err
+	}
+
+	aspectRatio := float64(size.Width) / float64(size.Height)
+
+	options := bimg.Options{
+		Quality: 80,
+		Type:    bimg.WEBP,
+	}
+
+	if w != 0 && h != 0 {
+		options.Width = w
+		options.Height = h
+	} else if w != 0 {
+		options.Width = w
+		options.Height = int(math.Round(float64(w) / aspectRatio))
+	} else if h != 0 {
+		options.Width = int(math.Round(float64(h) * aspectRatio))
+		options.Height = h
+	}
+
+	newImage, err := originalImage.Process(options)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(outputPath, newImage, 0644)
+	if err != nil {
+		return err
+	}
+
+	end := time.Now()
+	fmt.Printf("%s 처리 시간: %v ms\n", fileName, end.Sub(start).Milliseconds())
+	return nil
+}
+
+func main() {
+	imagePaths := []string{
+		"/app/assets/1.jpg",
+		"/app/assets/2.jpg",
+		"/app/assets/3.jpg",
+		"/app/assets/4.jpg",
+		"/app/assets/5.jpg",
+		"/app/assets/6.jpg",
+	}
+
+	err := os.MkdirAll("./output/bimg", 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	
+	totalStart := time.Now()
+
+	for _, path := range imagePaths {
+		wg.Add(1)
+		go func(p string) {
+			defer wg.Done()
+			if err := processImage(p, 0, 0); err != nil {
+				fmt.Printf("이미지 처리 중 오류 발생 (%s): %v\n", p, err)
+			}
+		}(path)
+	}
+
+	wg.Wait()
+
+	totalEnd := time.Now()
+	fmt.Printf("\n전체 처리 시간: %v ms\n", totalEnd.Sub(totalStart).Milliseconds())
+	fmt.Println("모든 이미지 처리 완료")
 }
