c6e6f5a4a8768680416b57c73b7eeb574d4730e1
dltmdrbtjd <dltmdrbtjd@gmail.com>
2025-01-04 16:31:50 +0900

    go init

---
+++ go-bimg/go.mod
@@ -0,0 +1,3 @@
+module github.com/dltmdrbtjd/image-bench
+
+go 1.23.4
---
+++ go-bimg/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// imagePath := []string{
+	// 	"./assets/1.jpg",
+	// 	"./assets/2.jpg",
+	// 	"./assets/3.jpg",
+	// 	"./assets/4.jpg",
+	// 	"./assets/5.jpg",
+	// 	"./assets/6.jpg",
+	// }
+
+	// outputPath := "./output/bimg"
+}
